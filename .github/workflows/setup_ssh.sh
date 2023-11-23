2c7f7489060296eebcc90ef4eeabf9019acf972a
Matthew Brett <matthew.brett@gmail.com>
2023-11-23 22:14:27 +0000

    Try Stefan's custom action

--- .github/workflows/setup_ssh.sh
+++
@@ -1,7 +0,0 @@
-ssh-keygen -b 2048 -t rsa -f sshkey -q -N ""
-HOME_DIR=$(eval echo "~${TEST_USER}")
-SSH_DIR=$HOME_DIR/.ssh
-sudo mkdir $SSH_DIR
-sudo cp sshkey.pub $SSH_DIR/authorized_keys
-TEST_GROUP=$(id -gn $TEST_USER)
-sudo chown -R ${TEST_USER}:${TEST_GROUP} $SSH_DIR
--- .github/workflows/ssh_func.sh
+++
@@ -1,3 +0,0 @@
-function ssh_user {
-    ssh -o StrictHostKeyChecking=no -i sshkey $TEST_USER@localhost ${SHELL_NAME} -c \"$@\"
-}
